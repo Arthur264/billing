e4de8d15211176e89ceeb3afced0515dbaa4eea3
Arthur Feduniv <afeduniv@lvivsoft.com>
2018-08-17 10:17:27 +0300

    add readme

---
+++ README.md
@@ -0,0 +1,2 @@
+Put '.csv' files in a 'tmp/' folder.
+Run 'python3 script.py'
--- script.py
+++ script.py
@@ -1,9 +1,11 @@
 import os
+import time
 from multiprocessing import Pool
 from app import Parser, FOLDER_NAME
 
 
 def main(file_name):
+    print('Parse file: {}'.format(file_name))
     parser = Parser(file_name)
     parser.start()
 
@@ -11,5 +13,8 @@ def main(file_name):
 if __name__ == '__main__':
     list_files = os.listdir(FOLDER_NAME)
     files = [file for file in list_files if file.endswith('.csv')]
+    time1 = time.time()
     with Pool(4) as p:
         p.map(main, files)
+    time2 = time.time()
+    print('Finish time: {}'.format((time2 - time1)))
